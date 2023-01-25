9e58350bc73332e6b39acff546f2ea9cbec61ee6
masiv <masiviady7@gmail.com>
2023-01-25 15:13:54 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var(
+		x int
+		y string
+		z bool
+)
+	x = 22
+	z = true
+	y ="Ady"
+	// y:=8
+	fmt.Printf("Bonjour j'ai %vans, j'ai m'appelle %v, et c'est vraiment %v",x,y,z)
+	
+}
