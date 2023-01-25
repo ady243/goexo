bdc6672b601f36c8920c8688a6823185aa2f0f2b
masiv <masiviady7@gmail.com>
2023-01-25 15:37:54 +0100

    les signes

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
+// package main
+
+// import "fmt"
+
+// func main(){
+// 	var(
+// 		b bool
+// 		s string
+// 		i int
+// 		u uint
+// 		u8 uint8
+// 		i8 int8
+// 		i16 int16
+// 		f float32
+
+// )
+// b = true
+// s  = "Ady"
+// i = -15
+// u = 10 // ne prend que le positif
+// u8 = 255 // o - 255
+// i8 = -4 // -128 - 127
+// i16 = 32767 // limite 32764
+// f = 4  //ne prend pas des virgules
+
+// 	// y:=8
+// 	fmt.Println(b,s,i,u,u8,i8,i16,f)
+
+// }
+
 package main
 
 import "fmt"
 
 func main(){
 	var(
-		x int
-		y string
-		z bool
-)
-	x = 22
-	z = true
-	y ="Ady"
-	// y:=8
-	fmt.Printf("Bonjour j'ai %vans, j'ai m'appelle %v, et c'est vraiment %v",x,y,z)
-	
+
+	)
+	fmt.Println(())
 }
